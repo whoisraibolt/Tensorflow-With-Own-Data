00bd49336a506ea58a79d74655952c91eac94a9e
Alexandra Raibolt <alexandra.raibolt@gmail.com>
2017-12-29 12:08:11 -0200

    Finish update

--- dateP/dateP.py
+++ dateP/dateP.py
@@ -9,9 +9,8 @@ import pickle
 import random
 
 class DateP(object):
-    def __init__(self, train_file,test_file, total_size):
-        self.train_file = train_file
-        self.test_file  = test_file
+    def __init__(self, file, total_size):
+        self.file = file
         self.total_size = total_size
 
     def date_process(self):
@@ -19,33 +18,8 @@ class DateP(object):
         all_data = []
         print('\nReading...\n')
 
-        # Read train.txt
-        with open(self.train_file) as f:
-            for line in f:
-
-                # Separate images and labels
-                image_file = line.strip().split(' ')[0]
-                label_file = line.strip().split(' ')[1]
-
-                '''
-                print 'Image file:',image_file
-                print 'Label file:',label_file
-                '''
-
-                # Append all labels
-                label.append(str(int(label_file)))
-
-                # To get path of images
-                path_image = 'dataset/'+image_file
-
-                # Get data location information and original size
-                data = data_pro(path_image, 0, 0, 48, 48)
-
-                # Append all data
-                all_data.append(data)
-
-        # Read test.txt     
-        with open(self.test_file) as f:
+        # Read train.txr or test.txt     
+        with open(self.file) as f:
             for line in f:
 
                 # Separate images and labels
@@ -65,7 +39,7 @@ class DateP(object):
 
                 # Get data location information and original size
                 # and send to data_pro()
-                data = data_pro(path_image, 0, 0, 48, 48)
+                data = data_pro(path_image, 0, 0, 384, 256)
 
                 # Append all data
                 all_data.append(data)
@@ -102,7 +76,7 @@ def data_pro(src, x1, y1, x2, y2):
     ROI = image_[x1:x2, y1:y2]
 
     # New size
-    size = (28, 28)
+    size = (48, 48)
 
     # Resize image
     image = cv2.resize(image_, size)
