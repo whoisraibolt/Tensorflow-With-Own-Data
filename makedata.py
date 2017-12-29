dea386eb5c1b4202393a8fd3e7d8945b65f659e4
Alexandra Raibolt <alexandra.raibolt@gmail.com>
2017-12-29 12:09:31 -0200

    Finish update

--- makedata.py
+++ makedata.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#joblib is usually significantly faster on large numpy arrays because it
+# has a special handling for the array buffers of the numpy datastructure
+
 # Imports
 from dateP.dateP import DateP
+from sklearn.externals import joblib
 import cv2
 import gzip
 import numpy as np
+import os
 import pickle
 import random
 import shutil
 
-# train.txt file path, should have 'imagepath label' as each row in .txt
-train_file = 'dataset/train.txt'
+# text.txt or train.txt file path, should have 'imagepath label' as each row in .txt
+file = 'dataset/train.txt'
 
-# text.txt same row	
-test_file = 'dataset/test.txt'   	
-
-# train, test and the total size of one image (28*28)
-Object = DateP(train_file, test_file, 784)
+# train or test and the total size of one image (48*48)
+Object = DateP(file, 2304)
 O1 = Object.date_process()
 d = O1
-
-# Generate the pkl.gz file
-p1 = pickle.dumps(d, 2)
-
-# Save as .gz
-file = gzip.open('dataset.pkl.gz', 'wb')
 
 # Print message
 print('writing...\n')
 
-# Write and then close file
-file.write(p1)
-file.close()
+# os.path.join([local], [filename])
+filename = os.path.join('/home/alexandra', 'dataset-training-set.pkl')
+
+# Generate the pkl file, save as .gz
+# Dumping in a gzip compressed file using a compress level of 3
+file = joblib.dump(d, filename + '.gz', compress=('gzip', 3))
 
 print('Done!\n')
